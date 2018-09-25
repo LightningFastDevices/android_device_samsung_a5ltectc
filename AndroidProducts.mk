b02600a29f9948523e36670ebe8e9ec1a2851812
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:54:10 -0400

    a5ltectc: Update AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_a5ltectc the AOSP way
    
    Change-Id: I0cb3d443b15ccf2a21a89b6e1a57039194779266

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,5 @@
 LOCAL_PATH := device/samsung/a5ltectc
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a5ltectc.mk
+	$(LOCAL_DIR)/omni_a5ltectc.mk \
+	$(LOCAL_DIR)/rr_a5ltectc.mk \
+	$(LOCAL_DIR)/lineage_a5ltectc.mk
