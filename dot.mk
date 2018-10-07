6ccae0ed8d52a3748d159b5e77a04c4060e35431
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:54 -0400

    a5ltectc: Add dotOS Makefile
    Change-Id: Id1314f40b6e3306c7adf490083842330e9b21c95

---
+++ dot.mk
@@ -0,0 +1,15 @@
+
+# Inherit from common
+$(call inherit-product, device/samsung/a5-common/dot.mk)
+
+$(call inherit-product, device/samsung/a5ltectc/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a5ltectc
+PRODUCT_NAME := dot_a5ltectc
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A5009
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
